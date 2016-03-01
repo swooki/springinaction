a5cc02f1c719d2f3b63a93a67a94051ba3f07561
Sungwook Kwon <swooki@gmail.com>
2016-02-29 23:18:12 -0500

    2.5

--- src/main/java/com/springinaction/ch2/soundsystem/CDPlayerConfig.java
+++ src/main/java/com/springinaction/ch2/soundsystem/CDPlayerConfig.java
@@ -1,10 +1,18 @@
 package com.springinaction.ch2.soundsystem;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
 public class CDPlayerConfig {
-
+	@Bean
+	public CompactDisc sgtPappers() {
+		return new SgtPappers();
+	}
+	
+	@Bean
+	public CDPlayer cdPlayer() {
+		return new CDPlayer(sgtPappers());
+	}
 }
