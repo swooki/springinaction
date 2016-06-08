affd125825753de1b14f1366d509537d2ee15086
KWONS <KWONS@JFSLT392660.msds.jfs.ohio.gov>
2016-06-08 13:59:56 -0400

    p143

--- src/test/java/spittr/web/HomeControllerTest.java
+++ src/test/java/spittr/web/HomeControllerTest.java
@@ -3,6 +3,10 @@ package spittr.web;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import spitter.web.HomeController;
 
@@ -10,6 +14,7 @@ public class HomeControllerTest {
     @Test
     public void testHomePage() throws Exception {
 	HomeController controller = new HomeController();
-	assertEquals("home", controller.home());
+	MockMvc mocMvc = MockMvcBuilders.standaloneSetup(controller).build();
+	mocMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(MockMvcResultMatchers.view().name("home"));
     }
 }
